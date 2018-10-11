12f73031faa92062e6996564a5863f05c4d2992e
Kydz Liu <boatkyd@gmail.com>
2018-10-11 15:00:41 +0800

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,18 +2,11 @@ export const environment = {
   production: false,
   apiEndpoint: 'https://dev-eos-api.tokenpad.io/',
   contractName: 'treasuregame',
-  // eosNet: {
-  //   protocol: 'http',
-  //   blockchain: 'eos',
-  //   host: 'jungle.cryptolions.io',
-  //   port: 18888,
-  //   chainId: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca'
-  // }
   eosNet: {
-    protocol: 'https',
+    protocol: 'http',
     blockchain: 'eos',
-    host: 'bp.cryptolions.io',
-    port: 443,
-    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+    host: 'jungle.cryptolions.io',
+    port: 18888,
+    chainId: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca'
   }
 };
