672bb7473a556f70b32c7b943ba9540f251e62c1
Ting <ting@57blocks.com>
2023-10-23 09:21:52 +0800

    test komment

--- src/app/helper-guide/helper-guide.component.ts
+++ src/app/helper-guide/helper-guide.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelperGuideComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('some changes!');
+  }
 
   ngOnInit() {
   }
